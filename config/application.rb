9d618bdc4245b1d04d5386779028565bd9ea4bbd
mmkarim <md.muzahidul.karim@framgia.com>
2017-11-20 16:46:51 +0700

    Added simple react components

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,16 @@ module ReactCrud
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  module Api
+    class Application < Rails::Application
+      config.middleware.use Rack::Cors do
+        allow do
+          origins "*"
+          resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+        end
+      end
+      config.active_record.raise_in_transactional_callbacks = true
+    end
+  end
 end
